8df17581379c536406fc6bd2f19a93d767cee407
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-01-17 13:32:13 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ setup(name='repoloadq',
       version='0.0.2',
       packages=find_packages(),
       install_requires=[
-          'celery==3.1.22',
           'pymongo==3.2.1',
       ],
       include_package_data=True,
